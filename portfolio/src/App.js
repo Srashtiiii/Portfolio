c8216e0ceb04eea5c9738e112b2319b5ff6ac773
srashtiiii <srashtig03@gmail.com>
2023-06-30 17:29:18 +0530

    commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
-
+import Header from './components/header/Header';
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
+    <div>
+      <Header />
     </div>
   );
 }
